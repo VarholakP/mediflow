6fe485370c5bee567aea770f560ede0e40d8b55c
Martin <laska.marrtin@gmail.com>
2025-11-29 20:12:46 +0100

    sidebar, appointment cards

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("whatever","./routes/whatever.tsx")
+    route("whatever","./routes/whatever.tsx"),
+    route("appointments", "./routes/appointments.tsx") 
 
 ] satisfies RouteConfig;
