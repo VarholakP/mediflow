a13b7fa377154f50a9f51c35c3e8a73cce8c3d1b
VarholakP <patrik.varholak@kosickaakademia.sk>
2025-11-29 22:24:08 +0100

    refactor messages

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -4,7 +4,7 @@ import { type RouteConfig, index, route, layout } from "@react-router/dev/routes
 export default [
     index("./routes/dashboard/home.tsx"),
     layout("./routes/dashboard/_layout.tsx", [
-        route("whatever", "./routes/dashboard/whatever.tsx"),
+        route("create-appointment", "./routes/dashboard/create-appointment.tsx"),
         route("appointments", "./routes/dashboard/appointments.tsx") 
     ])
 ] satisfies RouteConfig;
